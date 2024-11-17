b8cb8fbb732c400957ed539eac015ad638f71e6e
rohitpv88 <166278360+rohitpv88@users.noreply.github.com>
2024-11-17 11:09:52 +0530

    Update diabetics_pred.py

--- diabetics_pred.py
+++ diabetics_pred.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pickle
-loading_model = pickle.load(open('diabetics_prediction.pkl','rb'))
+loading_model = pickle.load(open('liver.pkl','rb'))
 input_data = (5,166,72,19,175,25.8,0.587,51)
 in_data_array = np.asarray(input_data)
 input_data_reshape = in_data_array.reshape(1, -1)
